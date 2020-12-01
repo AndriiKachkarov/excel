352e657fedfee91d49c3694ad3e4344e5461fdad
AndriyKachkarov <Andriy.Kachkarov@gmail.com>
2020-12-01 15:55:58 +0200

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.temlate';
-import {$} from '@core/dom';
+import {tableResizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
@@ -21,37 +22,8 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event) {
-        if (event.target.dataset.resize) {
-            const $resizer = $(event.target);
-            const $parent = $resizer.closest('[data-type="resizable"]');
-            const coords = $parent.getCoords();
-            const type = $resizer.data.resize;
-
-            $resizer.css({opacity: 1});
-
-            if (type === 'col') {
-                const colID = $parent.data.col;
-                const colArr = this.$root.findAll(`[data-col="${colID}"]`);
-
-                document.onmousemove = (e) => {
-                    const delta = e.pageX - coords.right;
-                    const value = coords.width + delta;
-                    $parent.css({width: value + 'px'});
-                    colArr.forEach((el) => el.style.width = value + 'px');
-                };
-            } else {
-                document.onmousemove = (e) => {
-                    const delta = e.pageY - coords.bottom;
-                    const value = coords.height + delta;
-                    $parent.css({height: value + 'px'});
-                };
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null;
-                document.onmouseup = null;
-                $resizer.css({opacity: 0});
-            };
+        if (shouldResize(event)) {
+            tableResizeHandler(this.$root, event);
         }
     }
 }
